4b9c65bcaca1ecb37541414994ab3ea34a536127
Ahmad Hamido <letraingrand1@gmail.com>
2022-11-30 14:13:20 +0100

    finalized the js and updated the css

--- Dog.js
+++ Dog.js
@@ -1,8 +1,11 @@
-
-
 class Dog {
     constructor(data) {
         Object.assign(this, data);
+    }
+
+    setMatchStatus(bool) {
+        this.hasBeenLiked = bool;
+        this.hasBeenSwiped = true;
     }
 
     getDogHtml() {
@@ -10,6 +13,7 @@ class Dog {
 
         return `
                 <div class="dog-details">
+                    <div class="badge" id="badge"></div>
                     <div class="dog-info">
                         <p class="dog-name" id="dog-name">${name}, ${age}</p>
                         <p class="dog-bio" id="dog-bio">${bio}</p>
--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 import dogs from './data.js';
 import Dog from './Dog.js';
 
-document.getElementById('like-btn').addEventListener('click', getNextDog);
-document.getElementById('dislike-btn').addEventListener('click', getNextDog);
-
 let currentDogIndex = 0;
 let currentDog = new Dog(dogs[currentDogIndex]);
+
+document.getElementById('like-btn').addEventListener('click', dogLiked);
+document.getElementById('dislike-btn').addEventListener('click', dogDisliked);
 
 function render () {
     document.getElementById('dog').innerHTML = currentDog.getDogHtml();
@@ -17,12 +17,37 @@ function getNextDog() {
     if (currentDogIndex < dogs.length -1) {
         currentDogIndex ++;
         currentDog = new Dog(dogs[currentDogIndex]);
+        render();
     } else {
         noMoreDogs();
     }
-    render();
 }
 
 function noMoreDogs() {
-    console.log('The array has reached its limit');
+    return document.getElementById('dog').innerHTML = `
+        <div class="change-area">
+            <img class="profile-pic" src="images/52D331D4-5FCC-4A02-AB72-C904F6AA00B3_1_105_c.jpeg">
+            <p class="no-more-dogs">There are no more dogs around you. Expand your discovery settings to see more dogs</p>
+            <button class="discovery-btn">DISCOVERY SETTINGS</button>
+        </div>
+        `
+}
+
+function dogLiked() {
+    if (currentDogIndex < dogs.length -1) {
+        currentDog.setMatchStatus(true);
+        document.getElementById('badge').innerHTML = 
+            `<img src="images/badge-like.png">`
+    }
+    setTimeout(getNextDog, 1000);
+}
+
+function dogDisliked() {
+    if (currentDogIndex < dogs.length -1) {
+        currentDog.setMatchStatus(false);
+        document.getElementById('badge').innerHTML = 
+            `<img src="images/badge-nope.png">`
+    }
+
+    setTimeout(getNextDog, 1000);
 }
