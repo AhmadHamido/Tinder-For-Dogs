12f20d4ae706c2ca1154120adc84945464befc70
Ahmad Hamido <letraingrand1@gmail.com>
2022-12-05 17:24:21 +0100

    Finalized the tinDog website

--- script.js
+++ script.js
@@ -24,7 +24,9 @@ function getNextDog() {
 }
 
 function noMoreDogs() {
-    return document.getElementById('dog').innerHTML = `
+    document.getElementById('tindog-btns').style.display = 'none';
+
+    document.getElementById('dog').innerHTML = `
         <div class="change-area">
             <img class="profile-pic" src="images/52D331D4-5FCC-4A02-AB72-C904F6AA00B3_1_105_c.jpeg">
             <p class="no-more-dogs">There are no more dogs around you. Expand your discovery settings to see more dogs</p>
@@ -34,16 +36,17 @@ function noMoreDogs() {
 }
 
 function dogLiked() {
-    if (currentDogIndex < dogs.length -1) {
+    if (currentDogIndex < dogs.length) {
         currentDog.setMatchStatus(true);
         document.getElementById('badge').innerHTML = 
             `<img src="images/badge-like.png">`
     }
+    
     setTimeout(getNextDog, 1000);
 }
 
 function dogDisliked() {
-    if (currentDogIndex < dogs.length -1) {
+    if (currentDogIndex < dogs.length) {
         currentDog.setMatchStatus(false);
         document.getElementById('badge').innerHTML = 
             `<img src="images/badge-nope.png">`
